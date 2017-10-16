fe92ca98fa4f71452357368c45bbde0d41cc74c4
saad <saad@128.4.63.28>
2017-10-16 15:52:29 -0400

    PS3

--- PS3/src/main/java/pkgCore/Deck.java
+++ PS3/src/main/java/pkgCore/Deck.java
@@ -2,9 +2,13 @@ package pkgCore;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.stream.Collectors;
 
 import pkgEnum.eRank;
 import pkgEnum.eSuit;
+import pkgException.DeckException;
+
+
 
 public class Deck {
 
@@ -19,19 +23,63 @@ public class Deck {
 		Collections.shuffle(cardsInDeck);
 	}
 
-	//TODO: Fix the Draw method so it throws an exception if the deck is empty
-	public Card Draw() {
+	//it throws an exception if the deck is empty
+	public Card Draw() throws DeckException {
+		if (this.cardsInDeck.size() == 0) {
+			throw new DeckException(this);
+		}
+		
+		
 		return cardsInDeck.remove(0);
 	}
 	
-	//TODO: Write an overloaded Draw method to Draw a card of a given eSuit
+	//Draw a card of a given eSuit
+	public Card DrawSuit(eSuit eSuit) {
+		for (Card card : this.cardsInDeck) {
+			if (card.geteSuit() == eSuit) {
+				cardsInDeck.remove(card);
+				return (card);
+			}
+		}
+		return null;
 	
-	//TODO: Write an overloaded Draw method to Draw a card of a given eRank
+	}
+	public Card DrawRank(eRank eRank) {
+		for (Card card : this.cardsInDeck) {
+			if (card.geteRank() == eRank) {
+				cardsInDeck.remove(card);
+				return (card);
+			}
+		}
+		return null;
+	}
 
-	//TODO: Write a method that will return the number of a given eSuit left in the deck.
-	
-	//TODO: Write a method that will return the number of a given eRank left in the deck.
-	
-	//TODO: Write a method that will return 0 or 1 if a given card is left in the deck.
-	
+	// return the number of a given eSuit left in the deck.
+	public int countSuit(eSuit eSuit) {
+		ArrayList<Card> eSuitLeft = cardsInDeck.stream().filter(c -> c.geteSuit() == eSuit)
+				.collect(Collectors.toCollection(ArrayList::new));
+
+		return eSuitLeft.size();
+	}
+
+	// return the number of a given eRank left in the deck.
+	public int countRank(eRank eRank) {
+		ArrayList<Card> eRankLeft = cardsInDeck.stream().filter(c -> c.geteRank() == eRank)
+				.collect(Collectors.toCollection(ArrayList::new));
+
+		return eRankLeft.size();
+	}
+
+	// return 0 or 1 if a given card is left in the deck.
+	public int FindCard(Card c) {
+		for (Card cards : cardsInDeck) {
+			if (cards == c)
+				return 1;
+		}
+		return 0;
+	}
 }
+
+	
+	
+	
--- PS3/src/test/java/pkgCore/DeckTest.java
+++ PS3/src/test/java/pkgCore/DeckTest.java
@@ -4,29 +4,50 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import pkgEnum.eRank;
+import pkgEnum.eSuit;
+import pkgException.DeckException;
+
+
 public class DeckTest {
 
-	@Test
-	public void TestEmptyDeck() {
+	@Test(expected = DeckException.class)
+	public void TestEmptyDeck() throws DeckException {
 		//TODO: Build a deck, draw until you get a DeckException
+		Deck Deck1 = new Deck();
+		for (int i = 0; i< 100; i++) {
+			Card c = Deck1.Draw();
+		}
 	}
 	
 	@Test
 	public void TestDrawSuit() {
 		//TODO: Build a deck, test the Draw(eSuit) method
+		Deck iDeck = new Deck();
+		Card c = iDeck.DrawSuit(eSuit.DIAMONDS);
+		assertTrue(c.geteSuit() == eSuit.DIAMONDS);
 	}
 	
 	@Test
 	public void TestDrawRank() {
 		//TODO: Build a deck, test the Draw(eRank) method
+		Deck d = new Deck();
+		Card card = d.DrawRank(pkgEnum.eRank.FIVE);
+		assertTrue(card.geteRank() == pkgEnum.eRank.FIVE);
 	}
 	
+	@Test
 	public void TestDeckRankCount() {
 		//TODO: Build a deck, test the DeckRankCount method
+		Deck d = new Deck();
+		assertEquals(d.countRank(pkgEnum.eRank.ACE), 4);
 	}
 	
 	public void TestDeckSuitCount() {
 		//TODO: Build a deck, test the DeckSuitCount method
+		Deck d = new Deck();
+		assertEquals(d.countSuit(pkgEnum.eSuit.DIAMONDS), 13);
+		
 	}
 
 }
